3760089acee8a99e2f1349528d99ee8493cd8d67
marcobdms <el_marco4@hotmail.com>
2026-04-20 00:37:42 +0200

    wojak

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,6 +10,7 @@ export default function Home() {
 
   return (
     <main className="home">
+      <img src="/npc.png" alt="NPC Wojak" className="home__logo" />
       <p className="home__title">{marca}</p>
       <p className="home__sub">Solo ropa. Solo existir.</p>
       <Link to="/catalogo" className="home__enter">Entrar</Link>
