6bc71a06978119306968798eb3e6d8e71c5c7af1
marcobdms <115955835+marcobdms@users.noreply.github.com>
2026-05-14 11:12:45 -0400

    Crear TopBanner.jsx

---
+++ frontend/src/components/TopBanner.jsx
@@ -0,0 +1,17 @@
+import './TopBanner.css'
+
+export default function TopBanner() {
+  return (
+    <div className="top-banner">
+      <div className="top-banner-track">
+        <span>
+          Aceptamos ZELLE • Divisas • Euros • Binance • Pago móvil • Transferencias internacionales • 20% OFF pagando en divisa • Precios reflejados en USD
+        </span>
+
+        <span>
+          Aceptamos ZELLE • Divisas • Euros • Binance • Pago móvil • Transferencias internacionales • 20% OFF pagando en divisa • Precios reflejados en USD
+        </span>
+      </div>
+    </div>
+  )
+}
